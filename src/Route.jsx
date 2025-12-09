2012aaa0f4dc29531c3b12e6880b6adeb3376d68
Gyeongbo Kang <gbkang@Gyeongboui-MacBookPro.local>
2025-12-09 11:27:11 +0900

    251209 11:27

--- src/Route.jsx
+++ src/Route.jsx
@@ -4,18 +4,22 @@ const Route = () => {
 
 	const destLat = 35.0716472
 	const destLng = 129.0573867
-	const Dest = '영도 목장원'
+	const Dest = '영도목장원'
 
 	const isMobile = /iPhone|iPad|Android/i.test(navigator.userAgent)
 
 	const openNaverMap = () => {
 		if (isMobile) {
 //			const appUrl = `nmap://map?lat=${destLat}&lng=${destLng}&appname=com.example.myapp`
-			const appUrl = `nmap://route/public?dlat=${destLat}&dlng${destLng}&dname=${Dest}&appname=com.my.app`
+			const appUrl = `nmap://route/car?dlat=${destLat}&dlng${destLng}&dname=${Dest}&appname=com.my.app`
 			window.location.href = appUrl
 		} else {
+			
+			const appUrl = `nmap://route/car?dlat=${destLat}&dlng${destLng}&dname=${Dest}&appname=com.my.app`
 			const webUrl = 'https:/map.naver.com/'
+			console.log({appUrl})
 			window.open(webUrl,'_blank');
+
 		}
 	}
 
