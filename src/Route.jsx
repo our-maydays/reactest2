85c59001940a32974a5146dffc74ac6a823fc39d
Gyeongbo Kang <gbkang@Gyeongboui-MacBookPro.local>
2025-12-09 13:21:35 +0900

    251209 13:21

--- src/Route.jsx
+++ src/Route.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import moment from 'moment'
+import 'moment/dist/locale/ko'
 
 const Route = () => {
+
+	moment.locale('ko')
 
 	const destLat = 35.0716472
 	const destLng = 129.0573867
@@ -8,27 +12,34 @@ const Route = () => {
 
 	const isMobile = /iPhone|iPad|Android/i.test(navigator.userAgent)
 
+
 	const openNaverMap = () => {
 		if (isMobile) {
-//			const appUrl = `nmap://map?lat=${destLat}&lng=${destLng}&appname=com.example.myapp`
 			const appUrl = `nmap://route/car?dlat=${destLat}&dlng=${destLng}&dname=${Dest}&appname=com.my.app`
 			window.location.href = appUrl
-//			UIApplication.shared.open(appUrl);
 		} else {
-			
-			const appUrl = `nmap://route/car?dlat=${destLat}&dlng${destLng}&dname=${Dest}&appname=com.my.app`
 			const webUrl = 'https:/map.naver.com/'
-			console.log({appUrl})
 			window.open(webUrl,'_blank');
-
 		}
 	}
 
+	const openKakaoMap = () => {
+		if (isMobile) {
+			const appUrl = `kakaomap://route?ep=${destLat},${destLng}&by=car`
+			window.location.href = appUrl
+		} else {
+			const webUrl = 'https://map.kakao.com/'
+			window.open(webUrl,'_blank')
+		}
+	}
 
 	return (
 		<div>
 			<button onClick={openNaverMap}>
-				네이버12:08
+				네이버{moment().format('HH:mm')}
+			</button>
+			<button onClick={openKakaoMap}>
+				카카오{moment().format('HH:mm')}
 			</button>
 		</div>
 	)
